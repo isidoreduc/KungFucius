2f83f62e489a176d1d44406f9919143c463a884a
isidoreduc <isidoreducasse79@gmail.com>
2019-01-31 13:29:36 +0100

    some material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { AppComponent } from './app.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 
@@ -21,7 +24,10 @@ import { MenuComponent } from './menu/menu.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
-    MatListModule
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
