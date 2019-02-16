ae1d42d3a54b52ce87abc005b7db754e7676f4d4
isidoreduc <isidoreducasse79@gmail.com>
2019-02-16 10:33:38 +0100

    menu component basic tests

--- src/app/menu/menu.component.spec.ts
+++ src/app/menu/menu.component.spec.ts
@@ -1,16 +1,47 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MenuComponent } from './menu.component';
+import { Observable, of } from 'rxjs';
+import { Dish } from '../shared/dish';
+import { DISHES } from '../shared/dishes';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatGridListModule, MatProgressSpinnerModule } from '@angular/material';
+import { RouterTestingModule } from '@angular/router/testing';
+import { DishService } from '../services/dish.service';
+import { baseURL } from '../shared/baseurl';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('MenuComponent', () => {
   let component: MenuComponent;
   let fixture: ComponentFixture<MenuComponent>;
 
   beforeEach(async(() => {
+
+    const dishServiceStub = {
+      getDishes: function(): Observable<Dish[]> {
+        return of(DISHES);
+      }
+    };
+
     TestBed.configureTestingModule({
-      declarations: [ MenuComponent ]
+      imports: [ BrowserAnimationsModule,
+        FlexLayoutModule,
+        MatGridListModule,
+        MatProgressSpinnerModule,
+        RouterTestingModule.withRoutes([{ path: 'menu', component: MenuComponent }])
+      ],
+      declarations: [ MenuComponent ],
+      providers: [
+        { provide: DishService, useValue: dishServiceStub },
+        { provide: 'baseURL', useValue: baseURL },
+      ]
     })
     .compileComponents();
+
+    const dishservice = TestBed.get(DishService);
+
   }));
 
   beforeEach(() => {
@@ -19,7 +50,27 @@ describe('MenuComponent', () => {
     fixture.detectChanges();
   });
 
+
+  // test 1
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  // test 2
+  it('dishes items equal to 4', () => {
+    expect(component.dishes.length).toBe(4);
+    expect(component.dishes[1].name).toBe('Zucchipakoda');
+    expect(component.dishes[3].featured).toBeFalsy();
+  });
+  // test 3
+  it('template test, h1 element contains the dish name in uppercase', () => {
+    fixture.detectChanges();
+
+    let de: DebugElement;
+    let el: HTMLElement;
+    de = fixture.debugElement.query(By.css('h1'));
+    el = de.nativeElement;
+
+    expect(el.textContent).toContain(DISHES[0].name.toUpperCase());
+
   });
 });
