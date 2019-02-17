f2c00ded56cc0da7446c8fa8581cbf46bac6de6a
isidoreduc <isidoreducasse79@gmail.com>
2019-02-17 14:03:20 +0100

    basic e2e testing

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,21 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo() {
-    return browser.get('/') as Promise<any>;
+  navigateTo(link: string) {
+    return browser.get(link) as Promise<any>;
   }
 
-  getTitleText() {
-    return element(by.css('app-root h1')).getText() as Promise<string>;
+
+
+  getParagraphText(selector: string) {
+    return element(by.css(selector)).getText();
+  }
+
+  getElement(selector: string) {
+    return element(by.css(selector));
+  }
+
+  getAllElements(selector: string) {
+    return element.all(by.css(selector));
   }
 }
